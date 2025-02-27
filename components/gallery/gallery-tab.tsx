2f3b8428b523410fa8d595f558e145b95c8aa9d9
Irina <irinagorea182002@gmail.com>
2025-02-27 15:41:21 +0200

    Individual product screen (Store)

---
+++ components/gallery/gallery-tab.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import { Tab } from '@headlessui/react';
+import { cn } from '@/lib/utils' 
+import { Image as ImageType } from '@/types';
+
+interface GalleryTabProps {
+    image: ImageType
+}
+
+const GalleryTab: React.FC<GalleryTabProps> = ({ image }) => {
+    return ( 
+        <Tab className="relative flex items-center 
+        justify-center rounded-md cursor-pointer aspect-square bg-white">
+            {({ selected }) => (
+                <div>
+                    <span className='absolute w-full h-full inset-0 overflow-hidden 
+                     aspect-square rounded-md'>
+                        <Image 
+                        fill alt="" 
+                        src={image.url} 
+                        className='object-cover object-center' />
+                    </span>
+                    <span className={cn("absolute inset-0 rounded-md ring-2 ring-offset-2", 
+                        selected ? "ring-black" : 'ring-transparent')} />
+                </div>
+            )}
+        </Tab>
+     );
+}
+ 
+export default GalleryTab;
